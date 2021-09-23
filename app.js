eabb3945b384e4f044f618897e7f80e2d9b3f5ad
Gunjan Thakur <Gunjant825@gmail.com>
2021-09-24 02:04:01 +0530

    final

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ function calculateProfitAndLoss(initial,quantity,current){
     if(initial>current){
         var loss = (initial-current)*quantity;
         var lossPercentage = (loss/initial)*100;
-        showOutput("Loss is" + loss + "and the percent is" + lossPercentage + "%");
+        showOutput("Loss is " + loss + " and the percent is " + lossPercentage + "%");
 
     }else if(current>initial){
         var profit = (current-initial)*quantity;
         var profitPercentage = (profit/initial)*100;
-        showOutput("The profit is"+ profit + "and the percentage is" + profitPercentage + "%");
+        showOutput("The profit is "+ profit + " and the percentage is " + profitPercentage + "%");
 
     }else{
         showOutput("No pain No gain :)");
